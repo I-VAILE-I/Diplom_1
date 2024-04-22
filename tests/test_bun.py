ad658ebfa49873f38ec32b66dc9b8b69a9a79cce
anchenko56@mail.ru <2001AndreyA>
2024-04-22 20:43:50 +0300

    Правки по ревью

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -5,8 +5,8 @@ class TestBun:
 
     def test_get_name_of_bun(self):
         buns = Bun(name='Фиолетовая булочка', price=100)
-        assert buns.name == 'Фиолетовая булочка'
+        assert buns.get_name() == 'Фиолетовая булочка'
 
     def test_get_price_of_bun(self):
         buns = Bun(name='Фиолетовая булочка', price=100)
-        assert buns.price == 100
+        assert buns.get_price() == 100
--- tests/test_ingredient.py
+++ tests/test_ingredient.py
@@ -5,12 +5,12 @@ class TestIngredient:
 
     def test_get_ingredient_type(self):
         ingredient = Ingredient(name='Сальса', price=1000, ingredient_type='Соус')
-        assert ingredient.type == 'Соус'
+        assert ingredient.get_type() == 'Соус'
 
     def test_get_ingredient_name(self):
         ingredient = Ingredient(name='Сальса', price=1000, ingredient_type='Соус')
-        assert ingredient.name == 'Сальса'
+        assert ingredient.get_name() == 'Сальса'
 
     def test_get_ingredient_price(self):
         ingredient = Ingredient(name='Сальса', price=1000, ingredient_type='Соус')
-        assert ingredient.price == 1000
+        assert ingredient.get_price() == 1000
